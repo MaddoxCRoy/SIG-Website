4b911700d1a4345ad0a85636b26474af1d9dfce4
Maddox Roy <maddo@Mac.lan>
2026-01-13 20:12:16 -0500

    All bugs worked out, code is still a little messy but everything is fully functional and looks perfect

--- src/components/home/Hero.jsx
+++ src/components/home/Hero.jsx
@@ -18,7 +18,7 @@ export default function Hero() {
         }}
       />
 
-      <div className="relative z-10 max-w-5xl mx-auto px-6 text-center">
+      <div className="relative z-10 max-w-5xl text-center">
         <motion.div
           initial={{ opacity: 0, y: 30 }}
           animate={{ opacity: 1, y: 0 }}
